69818a6035cf596047a509d0b75360f3b1630ec9
Hallie Gurr <halliegurr@gmail.com>
2025-08-23 23:10:54 -0700

    Update covid_vaccination_trends.R

--- covid_vaccination_trends.R
+++ covid_vaccination_trends.R
@@ -21,7 +21,7 @@
 setwd("C:/Users/...") # Replace the content within the quotes with your selected working directory
 getwd()
 
-# Load Required Packages #
+# Install and Load Required Packages #
 ## Install
 install.packages("readr")
 install.packages("dplyr")
