33cb50460957e4414d15623b4776973c16939593
Hallie Gurr <halliegurr@gmail.com>
2025-08-23 23:07:37 -0700

    Update covid_vaccination_trends.R

--- covid_vaccination_trends.R
+++ covid_vaccination_trends.R
@@ -22,37 +22,56 @@ setwd("C:/Users/...") # Replace the content within the quotes with your selected
 getwd()
 
 # Load Required Packages #
+## Install
+install.packages("readr")
+install.packages("dplyr")
+install.packages("tidyr")
+## Load
 library(readr)
 library(dplyr)
 library(tidyr)
 
 # Import and Explore Dataset #
+## Import
 covid <- read_csv("owid-covid-data.csv")
-# Quick Data Overview
+## Quick Data Overview
 str(covid)
 head(covid)
+
+# Define Variables of Interest #
+# 1. "people_fully_vaccinated" — Cumulative number of fully vaccinated individuals
+# 2. "new_vaccinations" — Daily new vaccinations administered
+# 3. "total_boosters" — Cumulative number of booster doses administered
+# 4. "date" — Date on which the data was recorded
+# 5. "location" — Country name where the data was recorded
 
 # --------------------
 # UNITED STATES
 # --------------------
 
 # Graph 1: People Fully Vaccinated #
-US1 <- filter(covid, covid$location == "United States" & covid$people_fully_vaccinated >= 0)
+## Date — x-axis
 US1date <- as.Date(US1$date, format ="%m/%d/%Y")
+## People Fully Vaccinated in the United States — y-axis
+US1 <- filter(covid, covid$location == "United States" & covid$people_fully_vaccinated >= 0)
 
 plot(US1date, US1$people_fully_vaccinated, main = "COVID-19: People Fully Vaccinated in the United States", 
      xlab = "Date", ylab = "Number of People Fully Vaccinated (in millions)", type = "l")
 
 # Graph 2: New Vaccinations Administered #
-US2 <- filter(covid, covid$location == "United States" & covid$new_vaccinations >= 0)
+## Date — x-axis
 US2date <- as.Date(US2$date, format ="%m/%d/%Y")
+## New Vaccinations Administered in the United States — y-axis
+US2 <- filter(covid, covid$location == "United States" & covid$new_vaccinations >= 0)
 
 plot(US2date, US2$new_vaccinations, main = "COVID 19: New Vaccinations Administered in the United States",
      xlab = "Date", ylab = "Number of People Administered New Vaccinations (in millions)", type = "l")
 
 # Graph 3: Total Boosters Administered #
-US3 <- filter(covid, covid$location == "United States" & covid$total_boosters >= 0)
+## Date — x-axis
 US3date <- as.Date(US3$date, format ="%m/%d/%Y")
+## Total Boosters Administrated in the United States — y-axis
+US3 <- filter(covid, covid$location == "United States" & covid$total_boosters >= 0)
 
 plot(US3date, US3$total_boosters, main = "COVID-19: Total Boosters Administrated in the United States",
      xlab = "Date", ylab = "Number of People Administered Boosters (in millions)", type = "l")
@@ -62,22 +81,28 @@ plot(US3date, US3$total_boosters, main = "COVID-19: Total Boosters Administrated
 # --------------------
 
 # Graph 1: People Fully Vaccinated #
-FR1 <- filter(covid, covid$location == "France" & covid$people_fully_vaccinated >= 0)
+## Date — x-axis
 FR1date <- as.Date(FR1$date, format ="%m/%d/%Y")
+## People Fully Vaccinated in France — y-axis
+FR1 <- filter(covid, covid$location == "France" & covid$people_fully_vaccinated >= 0)
 
 plot(FR1date, FR1$people_fully_vaccinated, main = "COVID-19: People Fully Vaccinated in France",
      xlab = "Date", ylab = "Number of People Fully Vaccinated (in millions)", type = "l")
 
 # Graph 2: New Vaccinations Administered #
-FR2 <- filter(covid, covid$location == "France" & covid$new_vaccinations >= 0)
+## Date — x-axis
 FR2date <- as.Date(FR2$date, format ="%m/%d/%Y")
+## New Vaccinations Administered in France — y-axis
+FR2 <- filter(covid, covid$location == "France" & covid$new_vaccinations >= 0)
 
 plot(FR2date, FR2$new_vaccinations, main = "COVID 19: New Vaccinations Administered in France",
      xlab = "Date", ylab = "Number of People Administered New Vaccinations (in millions)", type = "l")
 
 # Graph 3: Total Boosters Administered #
-FR3 <- filter(covid, covid$location == "France" & covid$total_boosters >= 0)
+## Date — x-axis
 FR3date <- as.Date(FR3$date, format ="%m/%d/%Y")
+## Total Boosters Administrated in France — y-axis
+FR3 <- filter(covid, covid$location == "France" & covid$total_boosters >= 0)
 
 plot(FR3date, FR3$total_boosters, main = "COVID-19: Total Boosters Administrated in France",
      xlab = "Date", ylab = "Number of People Administered Boosters (in millions)", type = "l")
@@ -87,22 +112,28 @@ plot(FR3date, FR3$total_boosters, main = "COVID-19: Total Boosters Administrated
 # --------------------
 
 # Graph 1: People Fully Vaccinated #
-DE1 <- filter(covid, covid$location == "Germany" & covid$people_fully_vaccinated >= 0)
+## Date — x-axis
 DE1date <- as.Date(DE1$date, format ="%m/%d/%Y")
+## People Fully Vaccinated in Germany — y-axis
+DE1 <- filter(covid, covid$location == "Germany" & covid$people_fully_vaccinated >= 0)
 
 plot(DE1date, DE1$people_fully_vaccinated, main = "COVID-19: People Fully Vaccinated in Germany",
      xlab = "Date", ylab = "Number of People Fully Vaccinated (in millions)", type = "l")
 
 # Graph 2: New Vaccinations Administered #
-DE2 <- filter(covid, covid$location == "Germany" & covid$new_vaccinations >= 0)
+## Date — x-axis
 DE2date <- as.Date(DE2$date, format ="%m/%d/%Y")
+## New Vaccinations Administered in Germany — y-axis
+DE2 <- filter(covid, covid$location == "Germany" & covid$new_vaccinations >= 0)
 
 plot(DE2date, DE2$new_vaccinations, main = "COVID 19: New Vaccinations Administered in Germany",
      xlab = "Date", ylab = "Number of People Administered New Vaccinations (in millions)", type = "l")
 
 # Graph 3: Total Boosters Administered #
-DE3 <- filter(covid, covid$location == "Germany" & covid$total_boosters >= 0)
+## Date — x-axis
 DE3date <- as.Date(DE3$date, format ="%m/%d/%Y")
+## Total Boosters Administrated in Germany — y-axis
+DE3 <- filter(covid, covid$location == "Germany" & covid$total_boosters >= 0)
 
 plot(DE3date, DE3$total_boosters, main = "COVID-19: Total Boosters Administrated in Germany",
      xlab = "Date", ylab = "Number of People Administered Boosters (in millions)", type = "l")
